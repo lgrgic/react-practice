5a583729f383c0464aed4cff7a103eb8b8941f3c
lgrgic <lukagrgic14@gmail.com>
2019-02-07 18:44:27 +0100

    Finish submit form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import Table from './Table';
+import Form from './Form';
 
 class App extends React.Component {
   state= {
-    characters: [
-      {
-        'name': 'Charlie',
-        'job': 'Janitor'
-      },
-      {
-        'name': 'Mac',
-        'job': 'Bouncer'
-      },
-      {
-        'name': 'Dee',
-        'job': 'Aspring actress'
-      },
-      {
-        'name': 'Dennis',
-        'job': 'Bartender'
-      }
-    ]
+    characters: []
   };
 
   removeCharacter = index => {
@@ -33,6 +17,10 @@ class App extends React.Component {
     });
   }
 
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
+
   render() {
     const { characters } = this.state;
 
@@ -42,6 +30,7 @@ class App extends React.Component {
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }

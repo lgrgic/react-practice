08d7eb5ef1e379dd022453ae5d5c1e5ec32a5c6a
lgrgic <lukagrgic14@gmail.com>
2019-02-07 19:16:53 +0100

    Updates

---
+++ precache-manifest.da867f7922dd8c3c84858147eff74058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "8a2c1f71a17e8641f883",
+    "url": "/react-practice/static/js/main.8a2c1f71.chunk.js"
+  },
+  {
+    "revision": "edf5071622be54c13aaa",
+    "url": "/react-practice/static/js/1.edf50716.chunk.js"
+  },
+  {
+    "revision": "8a2c1f71a17e8641f883",
+    "url": "/react-practice/static/css/main.8098ca14.chunk.css"
+  },
+  {
+    "revision": "a6473fc4a33832c7b3c74f9547c1662e",
+    "url": "/react-practice/index.html"
+  }
+];
